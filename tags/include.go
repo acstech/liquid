0b6df12bd52b0bbf7582a72085662ffb5511e020
Matt Golus <matt.golus@acstechnologies.com>
2015-08-12 16:44:43 -0400

    Added support for using both the "with" and "for" keywords when using the include tag.
    For example:
    
    {% include 'product' with products[0] %}
    
    and
    
    {% include 'product' for products %}

--- tags/include.go
+++ tags/include.go
@@ -2,6 +2,8 @@ package tags
 
 import (
 	"io"
+	"path/filepath"
+	"strings"
 
 	"github.com/acstech/liquid/core"
 )
@@ -15,13 +17,27 @@ func IncludeFactory(p *core.Parser, config *core.Configuration) (core.Tag, error
 	if value == nil {
 		return nil, p.Error("Invalid include value")
 	}
+
+	scopeType := p.ReadName()
+	var scope core.Value
+	if scopeType == "with" || scopeType == "for" {
+		scope, err = p.ReadValue()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		scopeType = ""
+	}
+
 	p.SkipPastTag()
-	return &Include{value, config.GetIncludeHandler()}, nil
+	return &Include{value, config.GetIncludeHandler(), scopeType, scope}, nil
 }
 
 type Include struct {
-	value   core.Value
-	handler core.IncludeHandler
+	value     core.Value
+	handler   core.IncludeHandler
+	scopeType string
+	scope     core.Value
 }
 
 func (i *Include) AddCode(code core.Code) {
@@ -33,10 +49,60 @@ func (i *Include) AddSibling(tag core.Tag) error {
 }
 
 func (i *Include) Execute(writer io.Writer, data map[string]interface{}) core.ExecuteState {
+	template := core.ToString(i.value.Resolve(data))
+	_, filename := filepath.Split(template)
+	extension := filepath.Ext(filename)
+	name := filename[0 : len(filename)-len(extension)]
+	contextVariableName := strings.ToLower(name)
+
+	var templateData = make([]map[string]interface{}, 1)
+
+	switch i.scopeType {
+	case "with":
+		scopedData := i.scope.Resolve(data)
+		templateData[0] = toMap(scopedData, contextVariableName)
+	case "for":
+		scopedData := i.scope.Resolve(data)
+
+		switch typed := scopedData.(type) {
+		case []interface{}:
+			templateData = make([]map[string]interface{}, len(typed))
+			for i, item := range typed {
+				templateData[i] = toMap(item, contextVariableName)
+			}
+		case []map[string]interface{}:
+			templateData = make([]map[string]interface{}, len(typed))
+			for i, item := range typed {
+				templateData[i] = toMap(item, contextVariableName)
+			}
+		default:
+			templateData[0] = toMap(scopedData, contextVariableName)
+		}
+	default:
+		templateData[0] = data
+	}
+
 	if i.handler != nil {
-		i.handler(core.ToString(i.value.Resolve(data)), writer, data)
+		for _, item := range templateData {
+			i.handler(template, writer, item)
+		}
 	}
 	return core.Normal
+}
+
+func toMap(data interface{}, contextVariableName string) map[string]interface{} {
+	if data == nil {
+		return nil
+	}
+
+	var context map[string]interface{}
+	if typed, ok := data.(map[string]interface{}); ok {
+		context = typed
+	} else {
+		context := make(map[string]interface{})
+		context[contextVariableName] = typed
+	}
+	return context
 }
 
 func (i *Include) Name() string {
